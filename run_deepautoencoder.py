66e049def487860277920251668c9f6bb99ddcbb
aravila <anderson.avila@gmail.com>
2016-12-05 20:25:28 -0500

    Includeing deep autoencoder

---
+++ run_deepautoencoder.py
@@ -0,0 +1,85 @@
+from models import BuildAudioModel, BuildVideoModel
+import sys
+import matplotlib
+matplotlib.use('Agg') 
+import matplotlib.pyplot as plot
+import tables
+import time
+
+def train_generator(filepath, no_train, batch_size):
+    while 1:
+        hdf5_file = tables.open_file(filepath, mode='r')
+        for i in range(0,round(no_train/batch_size)):
+            data = hdf5_file.root.train[i*batch_size:i*batch_size+batch_size]
+            yield (data, data)
+        hdf5_file.close()
+
+def validation_generator(filepath, no_valid, batch_size):
+    while 1:
+        hdf5_file = tables.open_file(filepath, mode='r')
+        for i in range(0, round(no_valid / batch_size)):
+            data = hdf5_file.root.valid[i*batch_size:i*batch_size+batch_size]
+            yield (data, data)
+        hdf5_file.close()
+
+def get_input_dim(filepath):
+    hdf5_file = tables.open_file(filepath, mode='r')
+    return hdf5_file.root.train.shape[1]
+
+def get_model(mode = 0, hdf5train = '', hdf5valid = '', no_train = 0, no_valid = 0, batch_size = 16, nepoch = 50, early = 5):
+
+    if (mode == 0):
+        model = BuildVideoModel()
+        modetype = "video"
+    else:
+        model = BuildAudioModel()
+        modetype = "audio"
+
+    input_dim = get_input_dim(hdf5train)
+
+    encoder, autoencoder = model.deepautoencoder(input_dim)
+
+    hist = autoencoder.fit_generator(train_generator(hdf5train, no_train, batch_size),
+                            nb_epoch=nepoch,
+                            samples_per_epoch=no_train,
+                            validation_data=validation_generator(hdf5valid, no_valid, batch_size*(no_valid/no_train)),
+                            nb_val_samples=no_valid,
+                            callbacks=[])
+    print(hist.history)
+    time_stamp = time.strftime("%Y-%m-%d_%H:%M")
+    encoder.save("../models/encoder_%s_%s.hdf5"%(modetype, time_stamp))
+    autoencoder.save("../models/autoencoder_%s_%s.hdf5"%(modetype, time_stamp))
+    fig = plot.figure()
+    lossFig = "../results/auto_%s_loss_%s.png"%(modetype, time_stamp)
+    plot.plot(hist.history['loss'], label='Train loss')
+#    plot.plot(hist.history['val_loss'], label='Valid loss')
+    plot.ylabel('Loss')
+    plot.xlabel('Epoches')
+    plot.legend(loc='upper left')
+    #plot.show()
+    fig.savefig(lossFig)
+
+
+def run_model():
+    if(len(sys.argv) < 6):
+        print("\nUsage:")
+        print("python run_autoencoder.py 0 \"'./vfeatures/vqoe_train.hdf5'\"  \"'./vfeatures/vqoe_valid.hdf5'\" 200000 50000 25 5")
+        print("Where")
+        print("param1: '0' for video and '1' for audio")
+        print("param2: hdf5 database path for training")
+        print("param3: hdf5 database path for validation")
+        print("param4: number of train instances")
+        print("param5: number of validation instances")
+        print("param6: batch size (default is 16)")
+        print("param7: number of epochs (default is 50)")
+        print("param8: early_stopping (default is 5)\n")
+        exit()
+
+    str = ""
+    for arg in sys.argv[1:]:
+        str += arg + ', '
+
+    cmd = "get_model(" + str[:-2] + ")"
+    eval(cmd)
+
+run_model()
